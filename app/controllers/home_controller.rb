2b9bb8650bdeb8fd4abc8692a67d2ae9241ccad0
Terence Lee <hone02@gmail.com>
2010-01-22 17:25:20 -0600

    show plugin count on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @count = Plugin.count
     @latest     = []
     @downloaded = []
     @updated    = []
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "when not logged in" do
+    context "and there are two plugins" do
+      setup do
+        2.times { Factory(:plugin) }
+      end
+
+      context "on GET index" do
+        setup do
+          get :index
+        end
+
+        should_assign_to(:count) { 2 }
+      end
+    end
   end
 end
